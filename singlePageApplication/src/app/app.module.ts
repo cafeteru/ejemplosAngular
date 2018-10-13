38111dd6a8d92f7e12e90668026cc94c747eb6bb
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2018-10-13 18:02:42 +0200

    Añade animaciones y rutas

--- singlePageApplication/src/app/app.module.ts
+++ singlePageApplication/src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
+// Rutas
+import {APP_ROUTING} from './app.routes';
+
+// Servicios
+
+
+// Componenetes
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './components/shared/navbar/navbar.component';
+import {HomeComponent} from './components/home/home.component';
+import {AboutComponent} from './components/about/about.component';
+import {HeroesComponent} from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
